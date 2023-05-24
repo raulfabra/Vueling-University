17a5f67f1e71d8727216193d6d01ddcb98dbac5c
raulfabra <raulfh_@hotmail.com>
2023-05-24 18:47:35 +0200

    Ejercicios completados

--- Ejercicios-javascript/ejercicio-7.js
+++ Ejercicios-javascript/ejercicio-7.js
@@ -1,2 +1,21 @@
 
-//NO ESTA BIEN HECHO
+calcularElPromedioDeEdadesMayoresDe18([2, 15, 45, 89, 42, 15, 26, 35, 29, 75, 2, 1, 6, 3, 16])
+
+function calcularElPromedioDeEdadesMayoresDe18 (edades){
+
+    let sumaEdades = 0
+    let sumaDeMayoresDe18 = 0;
+
+    for (let i = 0; i < edades.length; i++){
+        if (edades[i] >= 18){
+            sumaEdades += edades[i];
+            sumaDeMayoresDe18++;
+        }
+    }
+
+    if (sumaDeMayoresDe18 === 0) return 0;
+
+    const promedio = sumaEdades / sumaDeMayoresDe18;
+
+    console.log(Math.floor(promedio))
+}
