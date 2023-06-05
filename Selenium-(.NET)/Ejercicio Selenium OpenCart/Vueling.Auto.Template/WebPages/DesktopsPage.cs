6f211c567f856dca25c71c07b78cef53620d162b
raulfabra <raulfh_@hotmail.com>
2023-06-05 18:19:43 +0200

    resubida el proyecto de Opencarts testing Selenium

---
+++ Selenium-(.NET)/Ejercicio Selenium OpenCart/Vueling.Auto.Template/WebPages/DesktopsPage.cs
@@ -0,0 +1,55 @@
+﻿using Proyecto.Auto.Template.SetUp;
+using Proyecto.Auto.Template.WebPages.Base;
+using OpenQA.Selenium;
+using System.Collections.Generic;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Proyecto.Auto.Template.Webpages
+{
+    public class DesktopsPage : CommonPage
+    {
+
+        //CONSTRUCTOR
+        public DesktopsPage(ISetUpWebDriver setUpWebDriver) : base(setUpWebDriver)
+        {
+        }
+
+        //Find IWebElement By, ID, Css, XPath
+
+        private IWebElement Articulos (int idArticle) => WebDriver.FindElementByXPath($"//h4/a[contains(@href, 'product_id={idArticle}')]");
+
+        private IWebElement MessageArticulos => WebDriver.FindElementByXPath("//h3");
+
+
+        protected override IWebElement ApartadosBusqueda => throw new System.NotImplementedException();
+
+
+
+        //FUNCTIONS
+
+        public DesktopsPage SeleccionarArticulo(int idArticle)
+        {
+            Articulos(idArticle).Click();
+            return this;
+        }
+
+        public DesktopsPage ValidateGetAllDesktops()
+        {
+            Assert.AreEqual("Refine Search", MessageArticulos.Text);
+            return this;
+        }
+
+    }
+}
+
+            //Jse2.ExecuteScript("arguments[0].click()", Links[link]);
+            ////act.Click(Links[link]).Build().Perform();
+        //public DesktopsPage SiguientePagina()
+        //{
+        //    //act.MoveToElement(Pagina2).Build().Perform();
+        //    Jse2.ExecuteScript("arguments[0].scrollIntoView()", Pagina2);
+        //    Thread.Sleep(1000);
+        //    Pagina2.Click();
+        //    return this;
+        //}
---
+++ Selenium-(.NET)/Ejercicio Selenium OpenCart/Vueling.Auto.Template/WebPages/LoginPage.cs
@@ -0,0 +1,83 @@
+﻿using Proyecto.Auto.Template.SetUp;
+using Proyecto.Auto.Template.WebPages.Base;
+using OpenQA.Selenium;
+using System.Threading;
+using OpenQA.Selenium.Support.UI;
+using System;
+using Proyecto.Auto.Template.Common;
+using NUnit.Framework;
+
+namespace OpenCart.Auto.Template.Webpages
+{
+    public class LoginPage : CommonPage
+    {
+
+        //Constructor
+        public LoginPage (ISetUpWebDriver setUpWebDriver) : base(setUpWebDriver)
+        {
+        }
+        
+        //Define WebElements by: Id, CssSelector or XPath
+
+        private IWebElement BtnAccount  => WebDriver.FindElementByXPath("//a[@title = 'My Account']");
+
+        private IWebElement BtnLogIn    => WebDriver.FindElementByCssSelector("//a[@title='My Account']//../ul//a[text()='Login']");
+
+        private IWebElement MessageLogged => WebDriver.FindElementByXPath("//div[@id='content']/h2[text()='My Orders']");
+       
+        private By SubtitleNewCustomer => By.TagName("h2");
+
+        private By SubtitleMyAccount => By.TagName("h2");
+
+        private IWebElement EmailAdress => WebDriver.FindElementById("input-email");
+
+        private IWebElement Password => WebDriver.FindElementById("input-password");
+
+        private IWebElement SetLogIn => WebDriver.FindElementByXPath("//input[@value= 'Login']");
+
+        private IWebElement BtnHome => WebDriver.FindElementByXPath("//a[text()='Your Store']");
+
+
+
+
+
+
+
+        protected override IWebElement ApartadosBusqueda => throw new System.NotImplementedException();
+        //private IWebElement BuscarButton => throw new System.NotImplementedException();
+
+
+        //Define functions and actions 
+
+        public LoginPage GetLogin(string email, string password)
+        {
+            new WebDriverWait(WebDriver, TimeSpan.FromSeconds(WaitTimeout)).Until(CustomExpectedConditions.ElementIsVisible(SubtitleNewCustomer));
+            EmailAdress.Click();
+            EmailAdress.SendKeys(email);
+            Password.Click();
+            Password.SendKeys(password);
+            SetLogIn.Click();
+            new WebDriverWait(WebDriver, TimeSpan.FromSeconds(WaitTimeout)).Until(CustomExpectedConditions.ElementIsVisible(SubtitleMyAccount));
+            
+
+            return this;
+        }
+
+        public LoginPage ValidateLogIn()
+        {
+            Assert.AreEqual("My Orders", MessageLogged.Text);
+            BtnHome.Click();
+            return this;
+        }
+
+
+    }
+}
+
+
+
+//new WebDriverWait(WebDriver, TimeSpan.FromSeconds(WaitTimeout)).Until(CustomExpectedConditions.ElementIsVisible(btnWelcome));
+//WebDriver.FindElement(By.Id("ensCloseBanner")).Click();
+            //act.SendKeys(Buscador, busqueda2).Build().Perform();
+            //Thread.Sleep(1000);
+            //Buscador.SendKeys(Keys.Return);
